ca4e38493bd425e44d82bf9c435388945b249d86
Onur Bayraktaroğlu <onurbayraktaroglu@gmail.com>
2012-08-01 16:18:22 +0300

    siparis sayfasi jquery yui

--- static/js/basehtml.js
+++ static/js/basehtml.js
@@ -1,4 +1,4 @@
- $(function(){
+$(function(){
     $("#slides").slides({
     	next: 'next',
     	prev: 'prev',
@@ -8,3 +8,63 @@
     });
     
 });
+
+$(document).ready(function(){
+	$('.tarife-ui').click(function(){
+		$('.sip-tar').fadeOut('fast');	
+		$('.tar-main').fadeIn('fast');
+	});
+	$('.siparis-ui').click(function(){
+		$('.tar-main').fadeOut('fast');	
+		$('.sip-tar').fadeIn('fast');
+	});
+});
+
+$(document).ready(function(){
+	if($('.sip-window').length) {
+		var maxHeight = 0;
+		$('.sip-window').each(function(){
+			currentHeight=$(this).height();
+			//console.log(currentHeight);
+			if (currentHeight > maxHeight){
+				maxHeight = currentHeight;
+			}
+		});
+		$('.sip-window').height(maxHeight);
+	}
+});
+
+YUI().use('transition', 'node', 'slider', function (Y) {
+    
+	Y.all(".menu-item").on("click", function(e) {
+		e.halt();
+		var node = e.target.get("tagName") == "A" ? e.target.get("parentNode") : e.target;
+		this.each(function(item) {
+			item.one("a").setStyle("color", "#000");
+		});
+		Y.one(".bg-orange").transition({
+			duration: 0.40,
+			left: node.getStyle("left")
+		});
+		node.one("a").transition({
+			color: '#fff'
+		});
+	});
+	
+	
+});
+
+
+	$(function() {
+		$( "#slider-range-min" ).slider({
+			range: "min",
+			value: 100,
+			min: 100,
+			max: 700,
+			slide: function( event, ui ) {
+				$( "#amount" ).val( "$" + ui.value );
+			}
+		});
+		$( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
+	});
+
